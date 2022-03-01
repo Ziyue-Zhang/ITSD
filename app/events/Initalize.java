8bc12c36ef411d6b738b93fa3c473b416dd657c0
zzy <2846728884@qq.com>
2022-03-01 13:27:12 +0800

    finish initial

--- app/events/Initalize.java
+++ app/events/Initalize.java
@@ -3,9 +3,13 @@ package events;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import akka.actor.ActorRef;
+import commands.BasicCommands;
 import demo.CheckMoveLogic;
 import demo.CommandDemo;
 import structures.GameState;
+import structures.basic.*;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
 
 /**
  * Indicates that both the core game loop in the browser is starting, meaning
@@ -27,6 +31,83 @@ public class Initalize implements EventProcessor{
 		gameState.gameInitalised = true;
 		
 		gameState.something = true;
+
+		//gameInitialization
+		BasicCommands.addPlayer1Notification(out, "GameInitialization", 2);
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+
+		//drawTile
+		for(int i = 0; i <= 8; i++){
+			for(int j = 0; j <= 4; j++){
+				Tile tile = BasicObjectBuilders.loadTile(i, j);
+				BasicCommands.drawTile(out, tile, 0);
+			}
+		}
+
+		//drawUnit
+		Tile tile_human = BasicObjectBuilders.loadTile(1,2);
+		Unit unit_human = GameState.return_Unit("humanAvatar");
+		unit_human.setPositionByTile(tile_human);
+		//GameState.unit.add(unit_human);
+		BasicCommands.drawUnit(out, unit_human, tile_human);
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}	
+		BasicCommands.setUnitAttack(out, unit_human, 2);
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}	
+		BasicCommands.setUnitHealth(out, unit_human, 20);
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+
+		Tile tile_ai = BasicObjectBuilders.loadTile(7,2);
+		Unit unit_ai = GameState.return_enemy_unit("aiAvatar");
+		unit_ai.setPositionByTile(tile_ai);
+		//GameState.aiUnit.add(unitai);
+		BasicCommands.drawUnit(out, unit_ai, tile_ai);
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}	
+		BasicCommands.setUnitAttack(out, unit_ai, 2);
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}	
+		BasicCommands.setUnitHealth(out, unit_ai, 20);
+		try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}	
+		
+		// setPlayer1Health
+		BasicCommands.addPlayer1Notification(out, "setPlayer1Health", 2);
+		Player humanPlayer = new Player(20, 0);
+		BasicCommands.setPlayer1Health(out, humanPlayer);
+		try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+
+		// setPlayer2Health
+		BasicCommands.addPlayer1Notification(out, "setPlayer2Health", 2);
+		Player aiPlayer = new Player(20, 0);
+		BasicCommands.setPlayer2Health(out, aiPlayer);
+		try {Thread.sleep(2000);} catch (InterruptedException e) {e.printStackTrace();}
+
+		// Mana
+		for (int m = 0; m<10; m++) {
+			BasicCommands.addPlayer1Notification(out, "setPlayer1Mana ("+m+")", 1);
+			humanPlayer.setMana(m);
+			BasicCommands.setPlayer1Mana(out, humanPlayer);
+			try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+		}
+
+		// Mana
+		for (int m = 0; m<10; m++) {
+			BasicCommands.addPlayer1Notification(out, "setPlayer2Mana ("+m+")", 1);
+			aiPlayer.setMana(m);
+			BasicCommands.setPlayer2Mana(out, aiPlayer);
+			try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+		}
+
+		//deck1CardsDefinition
+		String[] deck1Cards = {
+			StaticConfFiles.c_comodo_charger,
+			StaticConfFiles.c_pureblade_enforcer,
+			StaticConfFiles.c_fire_spitter,
+		};
+
+		for (int i = 0; i <= 2; i++) {
+			// drawCard
+			Card card = BasicObjectBuilders.loadCard(deck1Cards[i], i, Card.class);
+			BasicCommands.drawCard(out, card, i, 0);
+			try {Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+		}
 		
 		// User 1 makes a change
 		//CommandDemo.executeDemo(out); // this executes the command demo, comment out this when implementing your solution
--- app/structures/GameState.java
+++ app/structures/GameState.java
@@ -1,5 +1,9 @@
 package structures;
 
+import structures.basic.Unit;
+import utils.BasicObjectBuilders;
+import utils.StaticConfFiles;
+
 /**
  * This class can be used to hold information about the on-going game.
  * Its created with the GameActor.
@@ -13,5 +17,58 @@ public class GameState {
 	public boolean gameInitalised = false;
 	
 	public boolean something = false;
+
+	public int[][] allocated = new int[9][5];
 	
+	public static Unit return_Unit(String name){
+		switch (name) {
+			case "humanAvatar" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.humanAvatar, 0, Unit.class);
+			case "comodo_charger" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_comodo_charger, 1, Unit.class);
+			case "azure_herald" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_azure_herald, 2, Unit.class);
+			case "azurite_lion" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_azurite_lion, 3, Unit.class);
+			case "fire_spitter" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_fire_spitter, 4, Unit.class);
+			case "hailstone_golem" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_hailstone_golem, 5, Unit.class);
+			case "ironcliff_guardian" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_ironcliff_guardian, 6, Unit.class);
+			case "pureblade_enforcer" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_pureblade_enforcer, 7, Unit.class);
+			case "silverguard_knight" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_silverguard_knight, 8, Unit.class);
+			default :
+		}
+		return null;
+	}
+
+
+	public static Unit return_enemy_unit(String name){
+		switch (name) {
+			case "aiAvatar" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.aiAvatar, 9, Unit.class);
+			case "blaze_hound" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_blaze_hound, 10, Unit.class);
+			case "bloodshard_golem" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_bloodshard_golem, 11, Unit.class);
+			case "hailstone_golemR" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_hailstone_golemR, 12, Unit.class);
+			case "planar_scout" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_planar_scout, 13, Unit.class);
+			case "pyromancer" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_pyromancer, 14, Unit.class);
+			case "rock_pulveriser" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_rock_pulveriser, 15, Unit.class);
+			case "serpenti" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_serpenti, 16, Unit.class);
+			case "windshrike" : 
+				return BasicObjectBuilders.loadUnit(StaticConfFiles.u_windshrike, 17, Unit.class);
+			default :
+		}
+		return null;
+	}
+
 }
